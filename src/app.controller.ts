d616d3952fe2d31f7d5f88a0c9b3609fcd743fd5
JeongMinSu <wjdalstn2752@naver.com>
2024-06-29 10:33:45 +0900

    minsu11

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -18,7 +18,6 @@ export class AppController {
     @Query('user') user: string,
     @Query('userVal') userVal: string,
     @Query('userPoint') userPoint: string,
-    @Query('userPoint') userPoint: string,
   ): Promise<string> {
     return this.appService.init(user, userVal, userPoint);
     return this.appService.init(user, userVal, userPoint);
@@ -41,23 +40,7 @@ async invokePoint(
 ): Promise<string> {
   return this.appService.invokePoint(sender, receiver, amount);
 }
-  @Get('/invoke')
-async invoke(
-  @Query('sender') sender: string,
-  @Query('receiver') receiver: string,
-  @Query('amount') amount: string,
-): Promise<string> {
-  return this.appService.invoke(sender, receiver, amount);
-}
 
-@Get('/invokePoint')
-async invokePoint(
-  @Query('sender') sender: string,
-  @Query('receiver') receiver: string,
-  @Query('amount') amount: string,
-): Promise<string> {
-  return this.appService.invokePoint(sender, receiver, amount);
-}
 
   @Get('/query')
   async query(
